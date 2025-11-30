79879a751b20e7291b1b3beb599b55c4fa6f10fd
PierreSQS <nomo_messina@web.de>
2025-11-30 17:50:22 +0100

    Sec6_Chap66: added TimeChatClientConfig and TimeController for chat functionality

---
+++ section06/springai/src/main/java/com/eazybytes/springai/config/TimeChatClientConfig.java
@@ -0,0 +1,34 @@
+package com.eazybytes.springai.config;
+
+import com.eazybytes.springai.advisors.TokenUsageAuditAdvisor;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.client.advisor.api.Advisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class TimeChatClientConfig {
+
+
+    // New to Sec6_Chap66
+    @Bean("timeChatClient")
+    public ChatClient chatClient(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory) {
+        // Introduced in Sec_Chap19
+        Advisor loggerAdvisor = new SimpleLoggerAdvisor();
+
+        // Introduced in Sec_Chap20
+        Advisor tokenUsageAdvisor = new TokenUsageAuditAdvisor();
+
+        // Introduced Sec_Chap44
+        Advisor memoryAdvisor = MessageChatMemoryAdvisor.builder(chatMemory).build();
+        return chatClientBuilder
+                .defaultAdvisors(List.of(loggerAdvisor, memoryAdvisor, tokenUsageAdvisor))
+
+                .build();
+    }
+}
---
+++ section06/springai/src/main/java/com/eazybytes/springai/controller/TimeController.java
@@ -0,0 +1,38 @@
+package com.eazybytes.springai.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.ai.chat.memory.ChatMemory.CONVERSATION_ID;
+
+@RestController
+@RequestMapping("/api/tools")
+public class TimeController {
+
+    private final ChatClient chatClient;
+
+    public TimeController(@Qualifier("timeChatClient") ChatClient chatClient) {
+        this.chatClient = chatClient;
+    }
+
+    @GetMapping("/local-time")
+    public ResponseEntity<String> chatMemory(@RequestHeader("username") String username,
+                                             @RequestParam("message") String message) {
+        return ResponseEntity.ok(chatClient.prompt()
+                .user(message)
+                .advisors(
+                        advisorSpec -> advisorSpec.param(CONVERSATION_ID, username)
+                )
+                .call().content());
+    }
+
+
+
+
+}
