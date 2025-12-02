666ec1f5b7bb3731472738048fa7e4316998a403
PierreSQS <nomo_messina@web.de>
2025-12-02 17:46:10 +0100

    Sec6_Chap71: enhanced HelpDeskTools with logging for ticket creation and status retrieval

--- section06/springai/src/main/java/com/eazybytes/springai/tools/HelpDeskTools.java
+++ section06/springai/src/main/java/com/eazybytes/springai/tools/HelpDeskTools.java
@@ -4,6 +4,7 @@ import com.eazybytes.springai.entity.HelpDeskTicket;
 import com.eazybytes.springai.model.TicketRequest;
 import com.eazybytes.springai.service.HelpDeskTicketService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.model.ToolContext;
 import org.springframework.ai.tool.annotation.Tool;
 import org.springframework.ai.tool.annotation.ToolParam;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class HelpDeskTools {
@@ -24,18 +26,24 @@ public class HelpDeskTools {
         // accessing tool context
         String username = (String)toolCtx.getContext().get("username");
 
+        log.info("### Creating ticket for user: {} with issue: {} ###", username, ticketRequest.issue());
         HelpDeskTicket ticket = ticketService.createTicket(ticketRequest, username);
+        log.info("### Ticket created with ID: {} for user: {} ###", ticket.getId(), username);
 
         return "Ticket created with ID: " + ticket.getId() + " for user: "
                 + username + " with issue: " + ticketRequest.issue();
     }
 
-    @Tool(description = "Fetch the status of Open tickets based on given username")
+    @Tool(description = "Fetch the status of tickets based on given username")
     public List<HelpDeskTicket> getTicketStatus(ToolContext toolContext) {
 
         String username = (String)toolContext.getContext().get("username");
 
-        return ticketService.getTicketsByUsername(username);
+        log.info("### Fetching tickets for user: {} ###", username);
+        List<HelpDeskTicket> ticketsByUsername = ticketService.getTicketsByUsername(username);
+        log.info("### Found tickets {} for user: {} ###", ticketsByUsername, username);
+
+        return ticketsByUsername;
 
     }
 
