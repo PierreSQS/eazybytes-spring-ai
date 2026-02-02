916a8e3be6cb818eaa341085388d1df142165ebe
PierreSQS <nomo_messina@web.de>
2026-02-03 00:36:54 +0100

    Sec7_Chap82-XX: made the chat-handler streamable

--- section07/mcpclient/src/main/java/com/eazybytes/mcpclient/controller/MCPClientController.java
+++ section07/mcpclient/src/main/java/com/eazybytes/mcpclient/controller/MCPClientController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
 @RequestMapping("api")
@@ -24,7 +25,7 @@ public class MCPClientController {
     }
 
     @GetMapping("mcp-client/chat")
-    public String chat(@RequestParam String message) {
-        return this.chatClient.prompt(message).call().content();
+    public Flux<String> chat(@RequestParam String message) {
+        return this.chatClient.prompt(message).stream().content();
     }
 }
