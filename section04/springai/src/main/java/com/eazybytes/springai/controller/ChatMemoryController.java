3929ff36fa622ad1aabbe9fe4bb9dd98ee6617c1
PierreSQS <nomo_messina@web.de>
2026-03-29 14:53:40 +0200

    Sec4_Chap47: enhanced chatMemory endpoint to include username header for conversation context

--- section04/springai/src/main/java/com/eazybytes/springai/controller/ChatMemoryController.java
+++ section04/springai/src/main/java/com/eazybytes/springai/controller/ChatMemoryController.java
@@ -1,6 +1,7 @@
 package com.eazybytes.springai.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,8 +17,13 @@ public class ChatMemoryController {
     }
 
     @GetMapping("/chat-memory")
-    public ResponseEntity<String> chatMemory(@RequestParam("message") String message) {
-        return ResponseEntity.ok(chatClient.prompt().user(message).call().content());
+    public ResponseEntity<String> chatMemory(@RequestParam("message") String message,
+                                             @RequestHeader("username") String username) {
+        return ResponseEntity.ok(chatClient.prompt()
+                .advisors(advisorSpec ->
+                        advisorSpec.param(ChatMemory.CONVERSATION_ID, username))
+                .user(message)
+                .call().content());
     }
 
 
