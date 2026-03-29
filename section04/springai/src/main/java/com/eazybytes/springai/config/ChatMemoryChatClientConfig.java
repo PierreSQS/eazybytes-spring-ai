0da6bfb0fc475d09320542ee9da560d135423242
PierreSQS <nomo_messina@web.de>
2026-03-29 09:27:58 +0200

    Sec4_Chap46: initial commit of Chap46

--- section04/springai/src/main/java/com/eazybytes/springai/config/ChatMemoryChatClientConfig.java
+++ section04/springai/src/main/java/com/eazybytes/springai/config/ChatMemoryChatClientConfig.java
@@ -1,14 +1,10 @@
 package com.eazybytes.springai.config;
 
-import com.eazybytes.springai.advisors.TokenUsageAuditAdvisor;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.client.advisor.api.Advisor;
 import org.springframework.ai.chat.memory.ChatMemory;
-import org.springframework.ai.chat.memory.MessageWindowChatMemory;
-import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
-import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,15 +13,14 @@ import java.util.List;
 @Configuration
 public class ChatMemoryChatClientConfig {
 
-    @Bean
-    ChatMemory chatMemory(JdbcChatMemoryRepository jdbcChatMemoryRepository) {
-        return MessageWindowChatMemory.builder().maxMessages(10)
-                .chatMemoryRepository(jdbcChatMemoryRepository).build();
-    }
-
     @Bean("chatMemoryChatClient")
     public ChatClient chatClient(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory) {
+        // logger advisor to log the interactions between the user and the model,
+        // which can be useful for debugging and monitoring purposes
         Advisor loggerAdvisor = new SimpleLoggerAdvisor();
+
+        // memory advisor to manage the chat memory and
+        // provide context to the model based on the conversation history
         Advisor memoryAdvisor = MessageChatMemoryAdvisor.builder(chatMemory).build();
         return chatClientBuilder
                 .defaultAdvisors(List.of(loggerAdvisor, memoryAdvisor))
