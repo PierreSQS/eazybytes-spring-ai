ca600d0c7042ef74455d48366de1bdaa99b235b7
PierreSQS <nomo_messina@web.de>
2026-04-02 13:19:24 +0200

    Sec5_Chap59: enhanced HRPolicyLoader to include text splitting for vector store

--- section05/springai/src/main/java/com/eazybytes/springai/rag/HRPolicyLoader.java
+++ section05/springai/src/main/java/com/eazybytes/springai/rag/HRPolicyLoader.java
@@ -4,6 +4,8 @@ package com.eazybytes.springai.rag;
 import jakarta.annotation.PostConstruct;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.reader.tika.TikaDocumentReader;
+import org.springframework.ai.transformer.splitter.TextSplitter;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
@@ -27,12 +29,22 @@ public class HRPolicyLoader {
 
     @PostConstruct
     public void loadHRPoliciesFromPDF() {
-        // Load the HR Policies PDF into the Vector Store
+        // Create a Tika-based reader that will extract text from the injected PDF resource:
         TikaDocumentReader tikaDocumentReader = new TikaDocumentReader(hrPoliciesRes);
 
+        // Read the extracted documents:
         List<Document> documentList = tikaDocumentReader.get();
 
-        vectorStore.add(documentList);
+        // Builds a token-based splitter
+        // with a fixed chunk size and an upper limit on chunks
+        TextSplitter textSplitter = TokenTextSplitter.builder()
+                .withChunkSize(100)
+                .withMaxNumChunks(400)
+                .build();
+
+        // loads the split documents into the vector store,
+        // making them available for retrieval during RAG operations
+        vectorStore.add(textSplitter.split(documentList));
 
     }
 
