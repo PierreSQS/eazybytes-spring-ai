3fca841b0f615cf2738f1f2e2800e257a3d2532a
PierreSQS <nomo_messina@web.de>
2026-04-02 18:03:53 +0200

    Sec5_Chap60: replaced boilerplate docs search code with New RetrievalAugmentationAdvisor

--- section05/springai/src/main/java/com/eazybytes/springai/config/ChatMemoryChatClientConfig.java
+++ section05/springai/src/main/java/com/eazybytes/springai/config/ChatMemoryChatClientConfig.java
@@ -1,5 +1,6 @@
 package com.eazybytes.springai.config;
 
+import com.eazybytes.springai.advisors.TokenUsageAuditAdvisor;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
@@ -7,6 +8,9 @@ import org.springframework.ai.chat.client.advisor.api.Advisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
+import org.springframework.ai.rag.advisor.RetrievalAugmentationAdvisor;
+import org.springframework.ai.rag.retrieval.search.VectorStoreDocumentRetriever;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -23,16 +27,41 @@ public class ChatMemoryChatClientConfig {
 
     // ChatClient with ChatMemory and RetrievalAugmentationAdvisor
     @Bean("chatMemoryChatClient")
-    public ChatClient chatClient(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory) {
+    public ChatClient chatClient(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory,
+                                 RetrievalAugmentationAdvisor retrievalAugmentationAdvisor) {
         // logger advisor to log the interactions between the user and the model,
         // which can be useful for debugging and monitoring purposes
         Advisor loggerAdvisor = new SimpleLoggerAdvisor();
+
+        // Token Usage advisor to track the number of tokens used
+        // in each interaction with the model,
+        Advisor tokenUsageAdvisor = new TokenUsageAuditAdvisor();
+
         // New to chapter (Sec5_Chap57)
         // memory advisor to manage the chat memory and
         // provide context to the model based on the conversation history
         Advisor memoryAdvisor = MessageChatMemoryAdvisor.builder(chatMemory).build();
+
         return chatClientBuilder
-                .defaultAdvisors(List.of(loggerAdvisor, memoryAdvisor))
+                .defaultAdvisors(List.of(loggerAdvisor, memoryAdvisor,
+                        tokenUsageAdvisor, retrievalAugmentationAdvisor))
+                .build();
+    }
+
+    // New to chapter (Sec5_Chap60)
+    // RetrievalAugmentationAdvisor for the Bean above
+    @Bean
+    public RetrievalAugmentationAdvisor retrievalAugmentationAdvisor(VectorStore vectorStore) {
+        return RetrievalAugmentationAdvisor.builder()
+                .documentRetriever(VectorStoreDocumentRetriever.builder()
+                        // Only retrieve documents with a similarity score above 0.5
+                        .similarityThreshold(0.5)
+                        // Retrieve the top 3 most relevant documents based on the similarity score
+                        .topK(3)
+                        // The vector store from which to retrieve relevant documents
+                        // based on the user's query
+                        .vectorStore(vectorStore)
+                        .build())
                 .build();
     }
 }
--- section05/springai/src/main/java/com/eazybytes/springai/controller/RAGController.java
+++ section05/springai/src/main/java/com/eazybytes/springai/controller/RAGController.java
@@ -3,8 +3,6 @@ package com.eazybytes.springai.controller;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
-import org.springframework.ai.document.Document;
-import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -16,23 +14,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 import static org.springframework.ai.chat.memory.ChatMemory.CONVERSATION_ID;
 
 @RestController
 @RequestMapping("/api/rag")
 public class RAGController {
 
+    private final ChatClient chatClient;
+    private final VectorStore vectorStore;
+
     @Value("classpath:/promptTemplates/systemPromptRandomDataTemplate.st")
     Resource promptTemplate;
 
     @Value("classpath:/promptTemplates/systemPromptTemplate.st")
     Resource hrSystemTemplate;
-
-    private final ChatClient chatClient;
-    private final VectorStore vectorStore;
 
     public RAGController(@Qualifier("chatMemoryChatClient") ChatClient chatClient, VectorStore vectorStore) {
         this.chatClient = chatClient;
@@ -44,6 +39,8 @@ public class RAGController {
     @GetMapping("/random/chat")
     public ResponseEntity<String> randomChat(@RequestHeader("username") String username,
                                      @RequestParam("message") String message) {
+        // the boilerplate code to search for similar documents
+        // is now replaced by an advisor (See QuestionAdvisor.java)
         String response = chatClient
                 .prompt()
                 .advisors(advisorSpec -> advisorSpec.param(ChatMemory.CONVERSATION_ID, username))
@@ -57,28 +54,9 @@ public class RAGController {
     @GetMapping("/document/chat")
     public ResponseEntity<String> documentChat(@RequestHeader("username") String username,
                                                @RequestParam("message") String message) {
-
-        // Configure the search request used later to query the vector store
-        SearchRequest searchRequest = SearchRequest.builder()
-                .query(message)           // text to match against vectors
-                .topK(3)                  // return up to 3 nearest neighbors
-                .similarityThreshold(0.5) // ignore results below this score
-                .build();
-
-        // hold the nearest docs from the vector store
-        List<Document> similarDocs =  vectorStore.similaritySearch(searchRequest);
-
-        // collect the text of the nearest documents returned by the vector store
-        // and join them into a single string.
-        String similarContext = similarDocs.stream()
-                .map(Document::getText)
-                .collect(Collectors.joining(System.lineSeparator()));
-
+        // the boilerplate code to search for similar documents
+        // is now replaced by an advisor (See chatMemoryChatClientConfig.java)
         String answer = chatClient.prompt()
-                .system(promptSystemSpec -> promptSystemSpec.text(hrSystemTemplate)
-                        // the similar documents are then inserted
-                        // into the prompt sent to the chat modell
-                        .param("documents", similarContext))
                 .advisors(a -> a.param(CONVERSATION_ID, username))
                 .user(message)
                 .call().content();
