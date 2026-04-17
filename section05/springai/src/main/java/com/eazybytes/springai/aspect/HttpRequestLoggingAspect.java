02600dbe7c0d09ba7916d652c220bdd730a75759
PierreSQS <nomo_messina@web.de>
2026-04-17 13:37:26 +0200

    Sec5_Chap64-XX: added HttpRequestLoggingAspect for logging HTTP requests and updated pom with AOP dependency

---
+++ section05/springai/src/main/java/com/eazybytes/springai/aspect/HttpRequestLoggingAspect.java
@@ -0,0 +1,40 @@
+package com.eazybytes.springai.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+
+// @Aspect declares the AOP role; @Component is still needed so Spring registers it as a bean
+@Aspect
+@Component
+public class HttpRequestLoggingAspect {
+
+    private static final Logger logger = LoggerFactory.getLogger(HttpRequestLoggingAspect.class);
+
+    // @Around (vs @Before) lets us call joinPoint.proceed() ourselves, keeping full control of the execution chain
+    @Around("within(@org.springframework.web.bind.annotation.RestController *)")
+    public Object logRequest(ProceedingJoinPoint joinPoint) throws Throwable {
+        // RequestContextHolder gives access to the current HTTP request without injecting HttpServletRequest everywhere
+        ServletRequestAttributes attrs =
+                (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+
+        if (attrs != null) {
+            HttpServletRequest request = attrs.getRequest();
+            logger.info("### HTTP {} {} | handler={} | args={} ###",
+                    request.getMethod(),
+                    request.getRequestURI(),
+                    joinPoint.getSignature().toShortString(),
+                    Arrays.toString(joinPoint.getArgs()));
+        }
+
+        return joinPoint.proceed();
+    }
+}
